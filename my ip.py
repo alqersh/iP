a5a76335461699d11229bcc991d3ff7c2dca1a5a
يوسف القرش <80622996+alqersh@users.noreply.github.com>
2021-07-09 13:12:44 -0700

    Update my ip.py

--- my ip.py
+++ my ip.py
@@ -1,3 +1,12 @@
+print('''
+ __  ____     __  _____ _____ ___
+ |  \/  \ \   / / |_   _|  __ \__ \
+ | \  / |\ \_/ /    | | | |__) | ) |
+ | |\/| | \   /     | | |  ___/ / /
+ | |  | |  | |     _| |_| |    |_|
+ |_|  |_|  |_|    |_____|_|    (_)
+                     by IG:@127.1.0.0.1
+''')
 import requests
 import json
 ip=requests.get("https://api.ipify.org/?format=json").json()['ip']
